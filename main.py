0912405853a09ef9ea47be7841c804d8350e608e
Paladin of Ioun <16229400+iounpaladin@users.noreply.github.com>
2026-07-06 18:01:11 -0700

    plan for computations

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ init_printing(use_latex=False)
 
 
 # Code goal: given cocharacter lambda, determine if i lambda(t) g in N(k) has a solution with i in I_p, g in G(O)
-# with given bounds on the powers of t appearing in i and g
+# with given bounds on the powers of t appearing in i and g          ^^^^ U_P(K)
 
 # Important: lowercase function names are Lie algebra level
 # Uppercase are Lie Group level
